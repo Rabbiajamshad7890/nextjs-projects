279b083cc206a4fe602c7d94afdb1b3121ceb935
Rabbiajamshad <hajikamran7890zz@gmail.com>
2025-10-08 06:19:46 -0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+import BlogsSection from '@/components/BlogsSection'
+import CommunitySection from '@/components/CommunitySection'
+import  CourseSection from '@/components/CourseSection'
+
+
+import DocumentationSection from '@/components/DocumentationSection'
+import HeroSection from '@/components/HeroSection'
+import P2PCloudCTABannerOriginal from '@/components/P2PCloudCTABannerOriginal'
+import ProductSection from '@/components/ProductSection'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <HeroSection />
+      <CourseSection/>
+      <ProductSection/>
+      <CommunitySection/>
+      <DocumentationSection/>
+      <BlogsSection />
+      <P2PCloudCTABannerOriginal />
+    </div>
+  )
+}
